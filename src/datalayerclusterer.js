2d054a09a81ff48a630b7e826e6a3b91e1b4759f
Nelson Antunes <nelson.antunes@gmail.com>
2015-05-06 00:56:03 +0100

    Refactors ClusterIcon to FeatureClusterIcon

--- src/datalayerclusterer.js
+++ src/datalayerclusterer.js
@@ -4,7 +4,7 @@
 
 /**
  * @name DataLayerClusterer for Google Maps v3
- * @version version 0.7.1
+ * @version version 0.7.2
  * @author Nelson Antunes
  *
  * The library creates and manages per-zoom-level clusters for large amounts of
@@ -570,7 +570,7 @@ function FeatureCluster(featureClusterer) {
 
     this.bounds_ = null;
 
-    this.clusterIcon_ = new ClusterIcon(this, featureClusterer.getStyles(),
+    this.clusterIcon_ = new FeatureClusterIcon(this, featureClusterer.getStyles(),
         featureClusterer.getGridSize());
 }
 
@@ -783,8 +783,8 @@ FeatureCluster.prototype.updateIcon = function() {
  * @constructor
  * @extends google.maps.OverlayView
  */
-function ClusterIcon(cluster, styles, opt_padding) {
-    DataLayerClusterer.extend(ClusterIcon, google.maps.OverlayView);
+function FeatureClusterIcon(cluster, styles, opt_padding) {
+    DataLayerClusterer.extend(FeatureClusterIcon, google.maps.OverlayView);
 
     this.styles_ = styles;
     this.padding_ = opt_padding || 0;
@@ -804,7 +804,7 @@ function ClusterIcon(cluster, styles, opt_padding) {
 /**
  * Hide the icon.
  */
-ClusterIcon.prototype.hide = function() {
+FeatureClusterIcon.prototype.hide = function() {
     if (this.div_) {
         this.div_.style.display = 'none';
     }
@@ -814,7 +814,7 @@ ClusterIcon.prototype.hide = function() {
 /**
  * Position and show the icon.
  */
-ClusterIcon.prototype.show = function() {
+FeatureClusterIcon.prototype.show = function() {
     if (this.div_) {
         var pos = this.getPosFromLatLng_(this.center_);
         this.div_.style.cssText = this.createCss(pos);
@@ -826,7 +826,7 @@ ClusterIcon.prototype.show = function() {
 /**
  * Remove the icon from the map
  */
-ClusterIcon.prototype.remove = function() {
+FeatureClusterIcon.prototype.remove = function() {
     this.setMap(null);
 };
 
@@ -835,7 +835,7 @@ ClusterIcon.prototype.remove = function() {
  *
  * @param {google.maps.LatLng} center The latlng to set as the center.
  */
-ClusterIcon.prototype.setCenter = function(center) {
+FeatureClusterIcon.prototype.setCenter = function(center) {
     this.center_ = center;
 };
 
@@ -846,7 +846,7 @@ ClusterIcon.prototype.setCenter = function(center) {
  * Adding the cluster icon to the dom.
  * @ignore
  */
-ClusterIcon.prototype.onAdd = function() {
+FeatureClusterIcon.prototype.onAdd = function() {
     this.div_ = document.createElement('DIV');
     if (this.visible_) {
         var pos = this.getPosFromLatLng_(this.center_);
@@ -867,7 +867,7 @@ ClusterIcon.prototype.onAdd = function() {
  * Draw the icon.
  * @ignore
  */
-ClusterIcon.prototype.draw = function() {
+FeatureClusterIcon.prototype.draw = function() {
     if (this.visible_) {
         var pos = this.getPosFromLatLng_(this.center_);
         this.div_.style.top = pos.y + 'px';
@@ -879,7 +879,7 @@ ClusterIcon.prototype.draw = function() {
  * Implementation of the onRemove interface.
  * @ignore
  */
-ClusterIcon.prototype.onRemove = function() {
+FeatureClusterIcon.prototype.onRemove = function() {
     if (this.div_ && this.div_.parentNode) {
         this.hide();
         this.div_.parentNode.removeChild(this.div_);
@@ -893,7 +893,7 @@ ClusterIcon.prototype.onRemove = function() {
 /**
  * Triggers the clusterclick event and zoom's if the option is set.
  */
-ClusterIcon.prototype.triggerClusterClick = function() {
+FeatureClusterIcon.prototype.triggerClusterClick = function() {
     var featureClusterer = this.cluster_.getDataLayerClusterer();
 
     // Trigger the clusterclick event.
@@ -912,7 +912,7 @@ ClusterIcon.prototype.triggerClusterClick = function() {
  * @return {google.maps.Point} The position in pixels.
  * @private
  */
-ClusterIcon.prototype.getPosFromLatLng_ = function(latlng) {
+FeatureClusterIcon.prototype.getPosFromLatLng_ = function(latlng) {
     var pos = this.getProjection().fromLatLngToDivPixel(latlng);
     pos.x -= parseInt(this.width_ / 2, 10);
     pos.y -= parseInt(this.height_ / 2, 10);
@@ -925,7 +925,7 @@ ClusterIcon.prototype.getPosFromLatLng_ = function(latlng) {
  * @param {google.maps.Point} pos The position.
  * @return {string} The css style text.
  */
-ClusterIcon.prototype.createCss = function(pos) {
+FeatureClusterIcon.prototype.createCss = function(pos) {
     var style = [];
     style.push('background-image:url(' + this.url_ + ');');
     var backgroundPosition = this.backgroundPosition_ ? this.backgroundPosition_ : '0 0';
@@ -964,7 +964,7 @@ ClusterIcon.prototype.createCss = function(pos) {
 /**
  * Sets the icon to the the styles.
  */
-ClusterIcon.prototype.useStyle = function() {
+FeatureClusterIcon.prototype.useStyle = function() {
     var index = Math.max(0, this.sums_.index - 1);
     index = Math.min(this.styles_.length - 1, index);
     var style = this.styles_[index];
@@ -984,7 +984,7 @@ ClusterIcon.prototype.useStyle = function() {
  *   'text': (string) The text to display in the icon.
  *   'index': (number) The style index of the icon.
  */
-ClusterIcon.prototype.setSums = function(sums) {
+FeatureClusterIcon.prototype.setSums = function(sums) {
     this.sums_ = sums;
     this.text_ = sums.text;
     this.index_ = sums.index;
